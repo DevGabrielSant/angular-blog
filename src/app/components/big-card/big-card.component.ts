c001ea82963c159dc7ba3af86ee2b0cf83bd0508
DevGabrielSant <Gabrielsantos1681@gmail.com>
2024-02-24 20:35:51 -0300

    content creat

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -11,16 +11,10 @@ import { Component, Input, OnInit } from '@angular/core';
     @Input()
     cardTitle:string=""
     @Input()
-    cardDescription:string=""
-    // 
-    // photoCover:string =""
-    // @Input()
-    // cardTitle:string= ""
-    // @Input()
-    // cardDescription:string =""
-    // @Input()
-    // Id:string="0"
-
+    cardDescription:string="" 
+    @Input()
+    Id:string="0"
+    
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "AINDA NÃO ESTÁ CORRENDO???",
+    "description": "ELE ESTA VIDO ATRÁS DE VOÇÊ",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTDkxl_rWGD_A7QopIr5jdb5THPnO01C-bhA&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"hhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTDkxl_rWGD_A7QopIr5jdb5THPnO01C-bhA&usqp=CAU"
+  }
+]
